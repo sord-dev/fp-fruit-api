0ed50772dbc73b4b146513cd78bd9de8ac9f5f20
Stef <stefansyrett17@gmail.com>
2023-02-28 15:00:43 +0000

    adding cors manually

--- server.js
+++ server.js
@@ -7,8 +7,16 @@ const { fruitsRouter, fruits } = require('./routers');
 const port = process.env.DEFAULT_PORT || 5000;
 const app = express();
 
+const allowCrossDomain = (req, res, next) => {
+    res.header(`Access-Control-Allow-Origin`, `example.com`);
+    res.header(`Access-Control-Allow-Methods`, `GET,PUT,POST,DELETE`);
+    res.header(`Access-Control-Allow-Headers`, `Content-Type`);
+    next();
+  };
+
 // --- middleware ---
 app.use(express.json());
+app.use(allowCrossDomain());
 app.use(expressLogger)
 
 // --- routes ---
